69ee6cc40d859902844c03c4c27ad7b11e6e9e12
nhjbest22 <nhjbest22@g.skku.edu>
2023-10-04 18:50:47 +0900

    commit main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List
 
-def twoSum(self, nums: List[int], target) -> List[int]:
+def twoSum(self, nums: List[int], target: int) -> List[int]:
     ans: List[int] = []
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
