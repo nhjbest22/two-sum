adc0160878ccfb181f182531d4f78b1ff8490bc8
nhjbest22 <nhjbest22@g.skku.edu>
2023-10-04 18:47:01 +0900

    commit main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def twoSum(self, nums: List[int], target) -> List[int]:
+    ans: List[int] = []
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if(nums[i] + nums[j] == target):
+                ans.extend([i, j])
+    return ans
+
+print(twoSum(None, [3,2,4], 6))
